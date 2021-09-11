e90eb659a57eecb578e6c7707827dd5cec0b79bc
Kiran Shila <me@kiranshila.com>
2021-09-11 12:49:44 -0700

    Only grab md files from posts dir

--- src/main/posts.clj
+++ src/main/posts.clj
@@ -1,12 +1,16 @@
 (ns main.posts
   (:require
    [cybermonday.core :as cm]
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
 
 (def posts-path "resources/public/posts")
 
 (defn gen-frontmatters []
   (->> (for [file (rest (file-seq (io/file posts-path)))
+             :when (and
+                    (= "md" (last (str/split (.getName file) #".")))
+                    (not (.isDirectory file)))
              :let [filename (.getName file)]]
          (assoc (cm/parse-front (slurp file)) :filename filename))
        (sort-by :date #(compare %2 %1))
